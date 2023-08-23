00972fac69d229c2d127d781e9d314bbecb4506f
eckoecko <142927274+eckoecko@users.noreply.github.com>
2023-08-23 07:21:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,2 @@
-#Credit: https://github.com/Area69Lab
-#setup.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
-
-#disable spotlight indexing
-sudo apt install python3 -y
 wget https://raw.githubusercontent.com/eckoecko/smd/main/mail.py
 python3 mail.py
